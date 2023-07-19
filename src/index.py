a84d3b8b641b14cf77be46c09f89f7a3eb0324c7
JnxyJInxed <64402575+JnxyJInxed@users.noreply.github.com>
2023-07-19 10:45:59 +0700

    Update index.py
    jadi static ikut hasil tru untuk tetsing

--- src/index.py
+++ src/index.py
@@ -509,8 +509,8 @@ def sign(ID, m, d_ID, Z_ID, Pub):
   # Pick a random number r_mid E Z/L* //r E {0,1,..l-1}
   # at random corresponding to m and ID
 
-  x_ID = getRmid()%n
-  # x_ID = 1351095810306068139124510697482103043048115624428965073737 #buat testing dengan bang hanang
+  # x_ID = getRmid()%n
+  x_ID = 2149849873984964502281777317092618629580601194310151160047 #buat testing dengan bang hanang
   print('x_ID = ', x_ID)
 
   S1 = muliplication_point(x_ID, Z_ID, p, a) #x_ID*Z_ID
